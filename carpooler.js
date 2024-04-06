c5e13b1ff839c6f1d1ebe6eb3dc180f26c094d87
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-06 20:46:35 +0000

    update

--- carpooler.js
+++ carpooler.js
@@ -11,12 +11,11 @@ sign_in_btn.addEventListener('click', () =>{
 });
 
 
-
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+// the below is not used but for reference
+document.getElementById("signupform").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent default form submission
-
   // Retrieve user input
-  const name = document.getElementById("nameInput").value.trim();
+  const name = document.getElementById("signupname").value.trim();
   const email = document.getElementById("emailInput").value.trim();
 
   // Validate user input
@@ -24,8 +23,19 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     alert("Please enter your name and email.");
     return;
   }
+  var i = 1;
+  while (localStorage.getItem("user" + i) !== null) {
+    i++;
+  }
+  localStorage.setItem("user" + i, name);
+  localStorage.setItem("email" + i, email);
 
+
+  // Retrieve
+  //document.getElementById("result").innerHTML = localStorage.getItem("user1");
+
+  //localStorage.clear();
   // Assume AJAX call to send login info to server and save in database
   // Redirect to another page after successful login
-  window.location.href = "eventSelection.html"; // Redirect to event selection page
+  window.location.href = "/result.html?driver=" + name; // Redirect to event selection page
 });
