458c190d16c88df679320cea5de83337a463ab2c
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-06 09:54:08 -0400

    Create role.js

---
+++ role.js
@@ -0,0 +1,19 @@
+// JavaScript for the webpage
+
+// Event listener for the Event Organizer button
+document.getElementById("eventOrganizerBtn").addEventListener("click", function() {
+  // Redirect to the Event Organizer page
+  window.location.href = "event_organizer.html";
+});
+
+// Event listener for the Scouter button
+document.getElementById("driverBtn").addEventListener("click", function() {
+  // Redirect to the Scouter page
+  window.location.href = "driver.html";
+});
+
+// Event listener for the Carpooler button
+document.getElementById("carpoolerBtn").addEventListener("click", function() {
+  // Redirect to the Carpooler page
+  window.location.href = "carpooler.html";
+});
