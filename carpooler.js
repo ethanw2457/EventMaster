80432a5d16ecbe3ee23c022871530b412ffedabc
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-06 14:24:15 +0000

    Update

--- carpooler.js
+++ carpooler.js
@@ -1,6 +1,17 @@
-// JavaScript for the carpooler page
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-// Event listener for the login form submission
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
+});
+
+
+
 document.getElementById("loginForm").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent default form submission
 
